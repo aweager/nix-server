1ca02190c2872f20f561827d1d3fa01f45722aed
Alex Eager <156651956+aweager@users.noreply.github.com>
2024-08-19 19:29:03 -0700

    Hack to avoid long lines from wrapping

--- executor/invoke-nix-develop.zsh
+++ executor/invoke-nix-develop.zsh
@@ -2,10 +2,14 @@
 
 exec 2>&1
 
+# Forcing to xterm-256color so I can limit line length
+export TERM=xterm-256color
+tput rmam
+
 IFS="" read -r
 nix_develop_args=(${(Q)${(z)REPLY}})
 
 printf '%s '  "Executing: nix develop" "$nix_develop_args[@]"
 echo
 
-exec nix develop "$@"
+exec nix develop "$nix_develop_args[@]"
